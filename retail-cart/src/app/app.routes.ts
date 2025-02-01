ad9f9cbb363fe3d3406beafb9261c02970a02185
Olajire Dominic <dominic.olajire@dentaldepartures.com>
2025-02-01 12:32:49 +0100

    feat: app layout and file structure setup

--- retail-cart/src/app/app.routes.ts
+++ retail-cart/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./layouts/main-layout/main-layout.routes').then((m) => m.routes),
+      }
+    ]
+  }
+];
---
+++ retail-cart/src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss'
+})
+export class MainLayoutComponent {
+
+  constructor() {
+    console.log("first")
+  }
+
+}
