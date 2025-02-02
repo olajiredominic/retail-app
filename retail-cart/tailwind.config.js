e03ac3cd304a998b939c90a70deae15c3d145072
Olajire Dominic <dominic.olajire@dentaldepartures.com>
2025-02-02 10:56:35 +0100

    feat: adds tailwind css for faster styling

---
+++ retail-cart/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#f5f5f5"
+      }
+    },
+  },
+  plugins: [],
+}
+
