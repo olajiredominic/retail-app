f154cad0858c1a06057323d325f7960b8978f173
Olajire Dominic <dominic.olajire@dentaldepartures.com>
2025-02-03 05:00:19 +0100

    feat: persist cart data in localstorge

--- retail-cart/src/app/services/cart/cart.service.ts
+++ retail-cart/src/app/services/cart/cart.service.ts
@@ -6,8 +6,22 @@ import { Product } from '../../models/product.model';
 @Injectable({ providedIn: 'root' })
 export class CartService {
   private cart: CartItem[] = [];
-  private cartSubject = new BehaviorSubject<CartItem[]>([]);
+  private cartSubject = new BehaviorSubject<CartItem[]>(this.loadCart());
   cart$ = this.cartSubject.asObservable();
+
+  constructor() {
+    this.cart = this.loadCart();
+    this.cartSubject.next(this.cart);
+  }
+
+  private saveCart() {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  private loadCart(): CartItem[] {
+    const cart = localStorage.getItem('cart');
+    return cart ? JSON.parse(cart) : [];
+  }
 
   addToCart(product: Product) {
     const item = this.cart.find(i => i.product.id === product.id);
@@ -16,11 +30,13 @@ export class CartService {
     } else {
       this.cart.push({ product, quantity: 1 });
     }
+    this.saveCart();
     this.cartSubject.next([...this.cart]);
   }
 
   removeFromCart(productId: number) {
     this.cart = this.cart.filter(item => item.product.id !== productId);
+    this.saveCart();
     this.cartSubject.next([...this.cart]);
   }
 
@@ -29,6 +45,7 @@ export class CartService {
     if (item) {
       item.quantity = quantity;
       if (quantity === 0) this.removeFromCart(productId);
+      this.saveCart();
       this.cartSubject.next([...this.cart]);
     }
   }
